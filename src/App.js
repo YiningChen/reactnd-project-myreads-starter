da79ea25df377104f889617bcd5c508dbbbf482e
yining <yining.chen@viacom.com>
2017-07-31 01:47:05 -0400

    set app state as BooksAPI.getAll()

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
   render () {
+    console.warn(this.state)
     return (
       <div className='app'>
         <Route exact path='/' component={ListBooks} />
         <Route path='/search' component={SearchBooks} />
       </div>
     )
+  }
+
+  componentDidMount () {
+    BooksAPI.getAll().then(data => this.setState(data))
   }
 }
 
